0dd7f257b7b3b4b5ac41e575d818a9c9e32e7cfc
Shamoon-Abbas <ibnabbasdev@gmail.com>
2025-07-14 00:49:52 +0500

    just completed the onboarding_6 except the progress indicator & the image height mismatch issue

--- lib/onboarding_6.dart
+++ lib/onboarding_6.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import 'onboarding_6.dart';
+import 'onboarding_1.dart';
 
-class Onboarding5 extends StatelessWidget {
-  const Onboarding5({super.key});
+class Onboarding6 extends StatelessWidget {
+  const Onboarding6({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +12,12 @@ class Onboarding5 extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(child: SvgPicture.asset("assets/images/onboard3.svg",
-              height: 523,)),
+            Center(child: SvgPicture.asset("assets/images/onboard4.svg",
+              height: 505,)),
             SizedBox(height: 70,),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal:20),
-              child: Text("Eat Well",style: TextStyle(
+              child: Text("Improve Sleep\nQuality",style: TextStyle(
                   fontSize: 30,
                   color: Colors.black,
                   fontFamily: 'bold'
@@ -25,7 +25,7 @@ class Onboarding5 extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-              child: Text("Let's start a healthy lifestyle with us, we can determine your diet every day. healthy eating is fun",
+              child: Text("Improve the quality of your sleep with us, good quality sleep can bring a good mood in the morning",
                 style: TextStyle(
                     fontSize: 16,
                     color: Color(0xFF7B6F72),
@@ -49,7 +49,7 @@ class Onboarding5 extends StatelessWidget {
                 ),
               ),Container(height: 65,width: 65,
                 child: FloatingActionButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Onboarding6()));
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Onboarding1()));
                 },child: Icon(Icons.arrow_forward_ios_rounded,color: Colors.white,size: 20),backgroundColor: Color(0xFF543753),shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(40)),),
               ),
             ]
